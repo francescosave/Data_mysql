888472eb787c95a839c42055eb9086ad2d2d486b
Francesco Ricci <francescosav.ricci@libero.it>
2017-08-18 16:40:02 +0100

    Aggiunta gestione modifica tabelle
    Aggiunta gestione comando alter table per modifica campi

--- src/MAIN.java
+++ src/MAIN.java
@@ -72,6 +72,16 @@ public class MAIN extends JFrame {
 	private JButton button_11;
 	private JButton button_12;
 	private JButton button_13;
+	private JButton btnUpdateRecord;
+	private JButton btnUpdateRecord_1;
+	private JButton btnUpdateRecord_2;
+	private JButton btnUpdateRecord_3;
+	private JButton btnUpdateRecord_4;
+	private JButton btnDeleteRecord;
+	private JButton btnDeleteRecord_1;
+	private JButton btnDeleteRecord_2;
+	private JButton btnDeleteRecord_3;
+	private JButton btnDeleteRecord_4;
 
 	/**
 	 * Launch the application.
@@ -100,19 +110,6 @@ public class MAIN extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		tFdriver = new JTextField();
-		tFdriver.setText("com.mysql.jdbc.Driver");
-		tFdriver.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		tFdriver.addFocusListener(new FocusAdapter() {
-			@Override
-			public void focusGained(FocusEvent arg0) {
-				lblDriver.setText("driver jdbc");
-			}
-		});
-		tFdriver.setBounds(10, 556, 304, 30);
-		contentPane.add(tFdriver);
-		tFdriver.setColumns(10);
-		
 		JToolBar toolBar = new JToolBar();
 		toolBar.setBounds(10, 0, 200, 16);
 		contentPane.add(toolBar);
@@ -120,107 +117,8 @@ public class MAIN extends JFrame {
 		lblDriver = new JLabel("driver");
 		toolBar.add(lblDriver);
 		
-		tFserver = new JTextField();
-		tFserver.setText("localhost");
-		tFserver.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		tFserver.addFocusListener(new FocusAdapter() {
-			@Override
-			public void focusGained(FocusEvent e) {
-				lblDriver.setText("server name mysql");
-			}
-		});
-		tFserver.setColumns(10);
-		tFserver.setBounds(51, 597, 122, 30);
-		contentPane.add(tFserver);
-		
-		tFport = new JTextField();
-		tFport.setText("3306");
-		tFport.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		tFport.addFocusListener(new FocusAdapter() {
-			@Override
-			public void focusGained(FocusEvent e) {
-				lblDriver.setText("server port mysql");
-			}
-		});
-		tFport.setColumns(10);
-		tFport.setBounds(10, 597, 40, 30);
-		contentPane.add(tFport);
-		
-		tFdatabase = new JTextField();
-		tFdatabase.setText("test");
-		tFdatabase.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		tFdatabase.addFocusListener(new FocusAdapter() {
-			@Override
-			public void focusGained(FocusEvent e) {
-				lblDriver.setText("database mysql");
-			}
-		});
-		tFdatabase.setColumns(10);
-		tFdatabase.setBounds(183, 597, 131, 30);
-		contentPane.add(tFdatabase);
-		
-		tFuser = new JTextField();
-		tFuser.setText("root");
-		tFuser.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		tFuser.setColumns(10);
-		tFuser.setBounds(324, 597, 81, 30);
-		contentPane.add(tFuser);
-		
-		tFconnectionString = new JTextField();
-		tFconnectionString.addFocusListener(new FocusAdapter() {
-			@Override
-			public void focusGained(FocusEvent e) {
-				lblDriver.setText("ConnectionString");
-			}
-		});
-		tFconnectionString.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		tFconnectionString.setColumns(10);
-		tFconnectionString.setBounds(10, 638, 304, 35);
-		tFconnectionString.setText("jdbc:mysql://"+tFserver.getText()+":"+tFport.getText()+"/"+tFdatabase.getText()+"?user="+tFuser.getText());
-		
-		contentPane.add(tFconnectionString);
-		
-		btnNewButton = new JButton("TEST");
-		btnNewButton.setForeground(Color.BLACK);
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-				if (VerificaDriver()=="VerificaDriver_OK"){
-					btnNewButton.setText("test OK");
-					btnNewButton.setForeground(Color.GREEN);
-				}else{
-					btnNewButton.setText("test KO");
-					btnNewButton.setForeground(Color.red);
-				}
-				
-				
-			}
-		});
-		btnNewButton.setBounds(324, 557, 81, 30);
-		contentPane.add(btnNewButton);
-		
-		button = new JButton("TEST");
-		button.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			
-				if (ConnettiDb()=="ConnettiDb_OK"){
-					button.setText("test OK");
-					button.setForeground(Color.green);
-				}else{
-					button.setText("test KO");
-					button.setForeground(Color.red);
-				}
-				
-				
-				
-			}
-		});
-		button.setForeground(Color.BLACK);
-		button.setBounds(324, 642, 81, 30);
-		contentPane.add(button);
-		
 		JPanel panel = new JPanel();
-		panel.setBounds(76, -11, 1289, 489);
+		panel.setBounds(10, 27, 1289, 646);
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
@@ -232,11 +130,11 @@ public class MAIN extends JFrame {
 			}
 		});
 		button_1.setForeground(Color.BLACK);
-		button_1.setBounds(229, 221, 98, 30);
+		button_1.setBounds(156, 221, 98, 30);
 		panel.add(button_1);
 		
 		scrollPane = new JScrollPane();
-		scrollPane.setBounds(21, 21, 585, 189);
+		scrollPane.setBounds(21, 21, 405, 189);
 		panel.add(scrollPane);
 		
 		textAreaSQL = new JTextArea();
@@ -244,7 +142,7 @@ public class MAIN extends JFrame {
 		textAreaSQL.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		
 		scrollPane_1 = new JScrollPane();
-		scrollPane_1.setBounds(21, 262, 585, 194);
+		scrollPane_1.setBounds(21, 262, 405, 194);
 		panel.add(scrollPane_1);
 		
 		textAreaLOG = new JTextArea();
@@ -257,7 +155,7 @@ public class MAIN extends JFrame {
 				textAreaSQL.setText("DROP TABLE registration;");
 			}
 		});
-		btnNewButton_1.setBounds(846, 23, 98, 30);
+		btnNewButton_1.setBounds(558, 21, 98, 30);
 		panel.add(btnNewButton_1);
 		
 		btnCreateTable = new JButton("CREATE TABLE");
@@ -272,7 +170,7 @@ public class MAIN extends JFrame {
 		                   " PRIMARY KEY ( id ))\n"); 
 			}
 		});
-		btnCreateTable.setBounds(954, 23, 105, 30);
+		btnCreateTable.setBounds(659, 21, 93, 30);
 		panel.add(btnCreateTable);
 		
 		btnInsertRecord = new JButton("INSERT RECORD");
@@ -289,21 +187,19 @@ public class MAIN extends JFrame {
 						
 			}
 		});
-		btnInsertRecord.setBounds(1069, 21, 127, 30);
+		btnInsertRecord.setBounds(867, 21, 116, 30);
 		panel.add(btnInsertRecord);
 		
 		button_2 = new JButton("DROP TABLE");
-		button_2.setEnabled(false);
 		button_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				textAreaSQL.setText("DROP TABLE ARTCOLI;");
+				textAreaSQL.setText("DROP TABLE ARTICOLI;");
 			}
 		});
-		button_2.setBounds(846, 66, 98, 30);
+		button_2.setBounds(558, 64, 98, 30);
 		panel.add(button_2);
 		
 		btnArticoli = new JButton("ARTICOLI");
-		btnArticoli.setEnabled(false);
 		btnArticoli.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				textAreaSQL.setText("CREATE TABLE ARTICOLI \n" + 
@@ -319,63 +215,246 @@ public class MAIN extends JFrame {
 						   "PRIMARY KEY ( CODICE ))\n"); 
 			}
 		});
-		btnArticoli.setBounds(954, 66, 105, 30);
+		btnArticoli.setBounds(659, 64, 93, 30);
 		panel.add(btnArticoli);
 		
 		button_4 = new JButton("INSERT RECORD");
-		button_4.setEnabled(false);
-		button_4.setBounds(1069, 64, 127, 30);
+		button_4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				textAreaSQL.setText("INSERT INTO TEST.ARTICOLI\n" +  
+						"(CODICE,DESCRIZIONE,DES_BREVE,CATEGORIA,PREZZO,TIPO,COLORE,ALQIVA,REPARTO)\n" + 
+						"VALUES\n" + 
+						"('CAFFE','CAFFE','CAFFE',1,1,'STD',0,'22',1);\n");
+				
+			}
+		});
+		button_4.setBounds(867, 64, 116, 30);
 		panel.add(button_4);
 		
 		button_5 = new JButton("DROP TABLE");
 		button_5.setEnabled(false);
-		button_5.setBounds(846, 109, 98, 30);
+		button_5.setBounds(558, 107, 98, 30);
 		panel.add(button_5);
 		
 		button_6 = new JButton("CREATE TABLE");
 		button_6.setEnabled(false);
-		button_6.setBounds(954, 109, 105, 30);
+		button_6.setBounds(659, 107, 93, 30);
 		panel.add(button_6);
 		
 		button_7 = new JButton("INSERT RECORD");
 		button_7.setEnabled(false);
-		button_7.setBounds(1069, 107, 127, 30);
+		button_7.setBounds(867, 107, 116, 30);
 		panel.add(button_7);
 		
 		button_8 = new JButton("DROP TABLE");
 		button_8.setEnabled(false);
-		button_8.setBounds(846, 152, 98, 30);
+		button_8.setBounds(558, 150, 98, 30);
 		panel.add(button_8);
 		
 		button_9 = new JButton("CREATE TABLE");
 		button_9.setEnabled(false);
-		button_9.setBounds(954, 152, 105, 30);
+		button_9.setBounds(659, 150, 93, 30);
 		panel.add(button_9);
 		
 		button_10 = new JButton("INSERT RECORD");
 		button_10.setEnabled(false);
-		button_10.setBounds(1069, 150, 127, 30);
+		button_10.setBounds(867, 150, 116, 30);
 		panel.add(button_10);
 		
 		button_11 = new JButton("DROP TABLE");
 		button_11.setEnabled(false);
-		button_11.setBounds(846, 195, 98, 30);
+		button_11.setBounds(558, 193, 98, 30);
 		panel.add(button_11);
 		
 		button_12 = new JButton("CREATE TABLE");
 		button_12.setEnabled(false);
-		button_12.setBounds(954, 195, 105, 30);
+		button_12.setBounds(659, 193, 93, 30);
 		panel.add(button_12);
 		
 		button_13 = new JButton("INSERT RECORD");
 		button_13.setEnabled(false);
-		button_13.setBounds(1069, 193, 127, 30);
+		button_13.setBounds(867, 193, 116, 30);
 		panel.add(button_13);
 		
-		JLabel lblNewLabel = new JLabel("TABLE REGISTATION");
+		JLabel lblNewLabel = new JLabel("REGISTATION");
 		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lblNewLabel.setBounds(645, 21, 191, 32);
+		lblNewLabel.setBounds(436, 21, 173, 32);
 		panel.add(lblNewLabel);
+		
+		JButton btnNewButton_2 = new JButton("ALTER TAB.");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				// PAROLA MODIFY PER MODIFICARE IL TIPO
+				// PAROLA CHANGE PER CAMBIARE IL NOME E VOLENDO ANCHE TIPO E ORDINE DI GRANDEZZA
+				//OBBLIGATORIO NONE_OLD NOME_NEW.ACCETTA ANCHE STESSO NOME
+				//PROBABILMENTE IN ORDINE DI PRESTAZIONI E PIU VELOCE MODIFY CHE NON CREA ALTRA COLONNA
+				textAreaSQL.setText(" ALTER TABLE ARTICOLI MODIFY  PREZZO DECIMAL(5,2) NOT NULL;");
+			}
+		});
+		btnNewButton_2.setBounds(755, 66, 89, 27);
+		panel.add(btnNewButton_2);
+		
+		tFdriver = new JTextField();
+		tFdriver.setBounds(21, 507, 304, 30);
+		panel.add(tFdriver);
+		tFdriver.setText("com.mysql.jdbc.Driver");
+		tFdriver.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		tFdriver.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent arg0) {
+				lblDriver.setText("driver jdbc");
+			}
+		});
+		tFdriver.setColumns(10);
+		
+		tFserver = new JTextField();
+		tFserver.setBounds(62, 547, 122, 30);
+		panel.add(tFserver);
+		tFserver.setText("localhost");
+		tFserver.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		tFserver.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				lblDriver.setText("server name mysql");
+			}
+		});
+		tFserver.setColumns(10);
+		
+		tFport = new JTextField();
+		tFport.setBounds(21, 547, 40, 30);
+		panel.add(tFport);
+		tFport.setText("3306");
+		tFport.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		tFport.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				lblDriver.setText("server port mysql");
+			}
+		});
+		tFport.setColumns(10);
+		
+		tFdatabase = new JTextField();
+		tFdatabase.setBounds(194, 547, 131, 30);
+		panel.add(tFdatabase);
+		tFdatabase.setText("test");
+		tFdatabase.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		tFdatabase.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				lblDriver.setText("database mysql");
+			}
+		});
+		tFdatabase.setColumns(10);
+		
+		tFuser = new JTextField();
+		tFuser.setBounds(335, 547, 81, 30);
+		panel.add(tFuser);
+		tFuser.setText("root");
+		tFuser.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		tFuser.setColumns(10);
+		
+		tFconnectionString = new JTextField();
+		tFconnectionString.setBounds(21, 588, 304, 35);
+		panel.add(tFconnectionString);
+		tFconnectionString.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				lblDriver.setText("ConnectionString");
+			}
+		});
+		tFconnectionString.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		tFconnectionString.setColumns(10);
+		tFconnectionString.setText("jdbc:mysql://"+tFserver.getText()+":"+tFport.getText()+"/"+tFdatabase.getText()+"?user="+tFuser.getText());
+		
+		btnNewButton = new JButton("TEST");
+		btnNewButton.setBounds(335, 507, 81, 30);
+		panel.add(btnNewButton);
+		btnNewButton.setForeground(Color.BLACK);
+		
+		button = new JButton("TEST");
+		button.setBounds(335, 592, 81, 30);
+		panel.add(button);
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			
+				if (ConnettiDb()=="ConnettiDb_OK"){
+					button.setText("test OK");
+					button.setForeground(Color.green);
+				}else{
+					button.setText("test KO");
+					button.setForeground(Color.red);
+				}
+				
+				
+				
+			}
+		});
+		button.setForeground(Color.BLACK);
+		
+		btnUpdateRecord = new JButton("UPDATE RECORD");
+		btnUpdateRecord.setEnabled(false);
+		btnUpdateRecord.setBounds(993, 21, 116, 30);
+		panel.add(btnUpdateRecord);
+		
+		btnUpdateRecord_1 = new JButton("UPDATE RECORD");
+		btnUpdateRecord_1.setEnabled(false);
+		btnUpdateRecord_1.setBounds(993, 64, 116, 30);
+		panel.add(btnUpdateRecord_1);
+		
+		btnUpdateRecord_2 = new JButton("UPDATE RECORD");
+		btnUpdateRecord_2.setEnabled(false);
+		btnUpdateRecord_2.setBounds(993, 107, 116, 30);
+		panel.add(btnUpdateRecord_2);
+		
+		btnUpdateRecord_3 = new JButton("UPDATE RECORD");
+		btnUpdateRecord_3.setEnabled(false);
+		btnUpdateRecord_3.setBounds(993, 150, 116, 30);
+		panel.add(btnUpdateRecord_3);
+		
+		btnUpdateRecord_4 = new JButton("UPDATE RECORD");
+		btnUpdateRecord_4.setEnabled(false);
+		btnUpdateRecord_4.setBounds(993, 193, 116, 30);
+		panel.add(btnUpdateRecord_4);
+		
+		btnDeleteRecord = new JButton("DELETE RECORD");
+		btnDeleteRecord.setEnabled(false);
+		btnDeleteRecord.setBounds(1121, 21, 116, 30);
+		panel.add(btnDeleteRecord);
+		
+		btnDeleteRecord_1 = new JButton("DELETE RECORD");
+		btnDeleteRecord_1.setEnabled(false);
+		btnDeleteRecord_1.setBounds(1121, 64, 116, 30);
+		panel.add(btnDeleteRecord_1);
+		
+		btnDeleteRecord_2 = new JButton("DELETE RECORD");
+		btnDeleteRecord_2.setEnabled(false);
+		btnDeleteRecord_2.setBounds(1121, 107, 116, 30);
+		panel.add(btnDeleteRecord_2);
+		
+		btnDeleteRecord_3 = new JButton("DELETE RECORD");
+		btnDeleteRecord_3.setEnabled(false);
+		btnDeleteRecord_3.setBounds(1121, 150, 116, 30);
+		panel.add(btnDeleteRecord_3);
+		
+		btnDeleteRecord_4 = new JButton("DELETE RECORD");
+		btnDeleteRecord_4.setEnabled(false);
+		btnDeleteRecord_4.setBounds(1121, 193, 116, 30);
+		panel.add(btnDeleteRecord_4);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				if (VerificaDriver()=="VerificaDriver_OK"){
+					btnNewButton.setText("test OK");
+					btnNewButton.setForeground(Color.GREEN);
+				}else{
+					btnNewButton.setText("test KO");
+					btnNewButton.setForeground(Color.red);
+				}
+				
+				
+			}
+		});
 	}
 	
 	private void ExecuteStatement() {
